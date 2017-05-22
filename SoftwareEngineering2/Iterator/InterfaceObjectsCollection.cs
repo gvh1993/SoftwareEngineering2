0be7f97396918fb098ce498492fff3a9963aaab9
DESKTOP-7BMGD7J\gertj <gertjanvanh@live.nl>
2017-05-22 07:55:30 +0200

    added beginning of iterator design pattern

---
+++ SoftwareEngineering2/Iterator/InterfaceObjectsCollection.cs
@@ -0,0 +1,23 @@
+﻿using SoftwareEngineering2.InterfaceObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineering2.Iterator
+{
+    class InterFaceObjectsCollection : IInterfaceObjectsCollection
+    {
+        private List<IInterfaceObject> _interfaceObjects;
+
+        public InterFaceObjectsCollection()
+        {
+            _interfaceObjects = new List<IInterfaceObject>();
+        }
+        public IIterator Iterator()
+        {
+            return new InterfaceObjectsIterator(_interfaceObjects);
+        }
+    }
+}
---
+++ SoftwareEngineering2/Iterator/InterfaceObjectsIterator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareEngineering2.InterfaceObjects;
+
+namespace SoftwareEngineering2.Iterator
+{
+    class InterfaceObjectsIterator : IIterator
+    {
+        private List<IInterfaceObject> _interfaceObjects;
+        private int position;
+        public InterfaceObjectsIterator(List<IInterfaceObject> interfaceObjects)
+        {
+            _interfaceObjects = interfaceObjects;
+        }
+        public bool HasNext()
+        {
+            while ()
+            {
+
+            }
+        }
+
+        public IInterfaceObject Next()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
