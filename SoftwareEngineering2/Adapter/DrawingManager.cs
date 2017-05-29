c613a1c5b9aee0ef1bfbee88f2d4a6505433a2d0
gvh1993 <gertjanvanh@live.nl>
2017-05-29 11:49:37 +0200

    created begin of adapter

---
+++ SoftwareEngineering2/Adapter/DrawingManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using SoftwareEngineering2.InterfaceObjects;
+
+namespace SoftwareEngineering2.Adapter
+{
+    class DrawingManager : IDrawingManager
+    {
+        public void DrawMonogame(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+
+            
+        }
+
+        public void DrawJavaFX()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SoftwareEngineering2/Adapter/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace SoftwareEngineering2.Adapter
+{
+    class InputManager : IInputManager
+    {
+        public MouseState GetMouseInput()
+        {
+            return new MouseState();
+        }
+
+        public KeyboardState GetKeyboardInput()
+        {
+            return new KeyboardState();
+        }
+    }
+}
